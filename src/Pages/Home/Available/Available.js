0f300f2436fea9047f7f8f590d4e0b1195ce81a2
mosheurpau <mosheur.pau@gamil.com>
2022-05-30 03:27:53 +0600

    added available container

--- src/Pages/Home/Available/Available.js
+++ src/Pages/Home/Available/Available.js
@@ -1,10 +1,52 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Available.css";
 
 const Available = () => {
   return (
-    <div>
-      <h2>This is Available</h2>
-    </div>
+    <Container>
+      <div className="text-center mt-5 p-5">
+        <h2 style={{ color: "#644F9C" }}>AVAILABLE FOR EVERYONE</h2>
+        <h5 style={{ fontWeight: "bold", color: "#404040" }}>
+          Here will be a text
+        </h5>
+      </div>
+      <Row className="justify-content-md-center mt-5 pt-3">
+        <Col lg="4" className="text-center mb-5">
+          <img
+            className="img-fluid"
+            src="https://i.ibb.co/rk4DQBh/available1.jpg"
+            alt=""
+          />
+          <h4 style={{ color: "#404040" }} className="py-3">
+            Text 1
+          </h4>
+          <button className="available-btn">Hello 1</button>
+        </Col>
+        <Col lg="4" className="text-center mb-5">
+          <img
+            className="img-fluid"
+            src="https://i.ibb.co/ZSQhzKQ/available2.jpg"
+            alt=""
+          />
+          <h4 style={{ color: "#404040" }} className="py-3">
+            Text 2
+          </h4>
+          <button className="available-btn">Hello 2</button>
+        </Col>
+        <Col lg="4" className="text-center mb-5">
+          <img
+            className="img-fluid"
+            src="https://i.ibb.co/FJgt6WD/available3.jpg"
+            alt=""
+          />
+          <h4 style={{ color: "#404040" }} className="py-3">
+            Text 3
+          </h4>
+          <button className="available-btn">Hello 3</button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Available from "../Available/Available";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Available></Available>
     </div>
   );
 };
